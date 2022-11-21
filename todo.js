5b0ae4984c1ea16b069ac39f1d2329aebee68015
iamqavi <76196705+iamqavi@users.noreply.github.com>
2022-11-21 21:53:09 +0530

    Update todo.js

--- todo.js
+++ todo.js
@@ -1,65 +1,30 @@
-require "date"
-require "active_record"
+const todoList = () => {
+  all = [];
+  const add = (todoItem) => {
+    all.push(todoItem);
+  };
+  const markAsComplete = (index) => {
+    all[index].completed = true;
+  };
 
-class Todo < ActiveRecord::Base
-  def overdue?
-    due_date < Date.today
-  end
+  const overdue = () => {
+    return all.filter(
+      (todo) => todo.dueDate < new Date().toLocaleDateString("en-CA")
+    );
+  };
 
-  def due_today?
-    due_date == Date.today
-  end
+  const dueToday = () => {
+    return all.filter(
+      (todo) => todo.dueDate == new Date().toLocaleDateString("en-CA")
+    );
+  };
 
-  def due_later?
-    due_date > Date.today
-  end
+  const dueLater = () => {
+    return all.filter(
+      (todo) => todo.dueDate > new Date().toLocaleDateString("en-CA")
+    );
+  };
 
-  def self.overdue
-    where("due_date < ?", Date.today)
-  end
-
-  def self.due_today
-    where("due_date = ?", Date.today)
-  end
-
-  def self.due_later
-    where("due_date > ?", Date.today)
-  end
-
-  def to_displayable_string
-    status = completed ? "[x]" : "[ ]"
-    date = due_today? ? nil : due_date
-    "#{id}. #{status} #{todo_text} #{date}"
-  end
-
-  def self.show_list
-    puts "My Todo-list\n\n"
-
-    puts "Overdue"
-    puts overdue.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-
-    puts "Due Today"
-    puts due_today.map { |todo| todo.to_displayable_string }
-    puts "\n\n"
-
-    puts "Due Later"
-    puts due_later.map { |todo| todo.to_displayable_string }
-  end
-
-  def self.add_task(task)
-    create!(
-      todo_text: task[:todo_text],
-      due_date: Date.today + task[:due_in_days],
-      completed: false,
-    )
-  end
-
-  def self.mark_as_complete(id)
-    todo = find(id)
-    todo.completed = true
-    todo.save!
-
-    puts todo.to_displayable_string
-  end
-end
+  return { all, add, markAsComplete, overdue, dueToday, dueLater };
+};
+module.exports = todoList;
